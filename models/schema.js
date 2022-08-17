4e528199293e75aa8e3fad866bd718904ff12679
ununique3 <j.horst77@gmail.com>
2022-08-17 16:27:23 -0600

    fixed part of the form in new  page

--- models/schema.js
+++ models/schema.js
@@ -1,7 +1,7 @@
 const mongoose = require ('mongoose')
 
 const newSchema = new mongoose.Schema ({
-    name: {type: String, require: true, unique: true},
+    name: {type: String, require: true},
     img: {type: String, unique: true},
     size: {type: String},
     style: {type:String},
