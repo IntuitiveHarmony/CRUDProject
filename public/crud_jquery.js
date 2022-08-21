b2ae0c1d7b2d4847b06d4657826e00a82a5e8cee
ununique3 <j.horst77@gmail.com>
2022-08-20 21:03:45 -0600

    cascading dropdown menus for edit and new pages

--- public/crud_jquery.js
+++ public/crud_jquery.js
@@ -205,7 +205,8 @@ $(() => {
   //-----------------------------------------------
   //       FILTER THE STYLES ON EDIT PAGE (1ST TIME)
   //-----------------------------------------------
-  $(`#filter`).hover(filterStyle)
+  // $(`#filter`).hover(filterStyle)
+  // $(`#filter`).on(`click`, filterStyle)
 
   //-----------------------------------------------
   //        HOVER OVER BUTTONS/LINKS
